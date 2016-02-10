dea86e7b0a0496db93f0d4dbd0030908c7b83cd9
Makoto HAMATSU <hamatsu.m@googlemail.com>
2016-02-10 18:05:04 +0900

    Update inspect.py
    change sample code for supporting image re-sizing

--- examples/inspection/inspect.py
+++ examples/inspection/inspect.py
@@ -7,6 +7,7 @@ import re
 import imp
 
 from PIL import Image
+import scipy.misc
 import cv2
 
 import six
@@ -20,40 +21,115 @@ def load_module(dir_name, symbol):
     (file, path, description) = imp.find_module(symbol, [dir_name])
     return imp.load_module(symbol, file, path, description)
 
-def read_image(path, model, mean_image, cropwidth, center=False, flip=False):
-    image = np.asarray(Image.fromarray(resize_image(path))).transpose(2, 0, 1)
-    if center:
-        top = left = cropwidth / 2
+def read_image(path, height, width, resize_mode = "squash", channels=3, flip=False):
+    """
+    Load an image from disk
+
+    Returns an np.ndarray (channels x width x height)
+
+    Arguments:
+    path -- path to an image on disk
+    width -- resize dimension
+    height -- resize dimension
+
+    Keyword arguments:
+    channels -- the PIL mode that the image should be converted to
+        (3 for color or 1 for grayscale)
+    resize_mode -- can be crop, squash, fill or half_crop
+    flip -- flag for flipping
+    """
+    
+    if channels == 1:
+        mode = "L"
     else:
-        top = random.randint(0, cropwidth - 1)
-        left = random.randint(0, cropwidth - 1)
-    bottom = model.insize + top
-    right = model.insize + left
-    image = image[:, top:bottom, left:right].astype(np.float32)
-    image -= mean_image[:, top:bottom, left:right]
-    image /= 255
+        mode = "RGB"
+        
+    image = Image.open(path)
+    image = image.convert(mode)
+    image = np.array(image)
+
+    ### Resize
+    interp = 'bilinear'
+    
+    width_ratio = float(image.shape[1]) / width
+    height_ratio = float(image.shape[0]) / height
+    if resize_mode == 'squash' or width_ratio == height_ratio:
+        return scipy.misc.imresize(image, (height, width), interp=interp)
+    elif resize_mode == 'crop':
+        # resize to smallest of ratios (relatively larger image), keeping aspect ratio
+        if width_ratio > height_ratio:
+            resize_height = height
+            resize_width = int(round(image.shape[1] / height_ratio))
+        else:
+            resize_width = width
+            resize_height = int(round(image.shape[0] / width_ratio))
+        image = scipy.misc.imresize(image, (resize_height, resize_width), interp=interp)
+
+        # chop off ends of dimension that is still too long
+        if width_ratio > height_ratio:
+            start = int(round((resize_width-width)/2.0))
+            return image[:,start:start+width]
+        else:
+            start = int(round((resize_height-height)/2.0))
+            return image[start:start+height,:]
+    else:
+        if resize_mode == 'fill':
+            # resize to biggest of ratios (relatively smaller image), keeping aspect ratio
+            if width_ratio > height_ratio:
+                resize_width = width
+                resize_height = int(round(image.shape[0] / width_ratio))
+                if (height - resize_height) % 2 == 1:
+                    resize_height += 1
+            else:
+                resize_height = height
+                resize_width = int(round(image.shape[1] / height_ratio))
+                if (width - resize_width) % 2 == 1:
+                    resize_width += 1
+            image = scipy.misc.imresize(image, (resize_height, resize_width), interp=interp)
+        elif resize_mode == 'half_crop':
+            # resize to average ratio keeping aspect ratio
+            new_ratio = (width_ratio + height_ratio) / 2.0
+            resize_width = int(round(image.shape[1] / new_ratio))
+            resize_height = int(round(image.shape[0] / new_ratio))
+            if width_ratio > height_ratio and (height - resize_height) % 2 == 1:
+                resize_height += 1
+            elif width_ratio < height_ratio and (width - resize_width) % 2 == 1:
+                resize_width += 1
+            image = scipy.misc.imresize(image, (resize_height, resize_width), interp=interp)
+            # chop off ends of dimension that is still too long
+            if width_ratio > height_ratio:
+                start = int(round((resize_width-width)/2.0))
+                image = image[:,start:start+width]
+            else:
+                start = int(round((resize_height-height)/2.0))
+                image = image[start:start+height,:]
+        else:
+            raise Exception('unrecognized resize_mode "%s"' % resize_mode)
+
+        # fill ends of dimension that is too short with random noise
+        if width_ratio > height_ratio:
+            padding = int((height - resize_height)/2)
+            noise_size = (padding, width)
+            if channels > 1:
+                noise_size += (channels,)
+            print noise_size
+            noise = np.random.randint(0, 255, noise_size).astype('uint8')
+            image = np.concatenate((noise, image, noise), axis=0)
+        else:
+            padding = (width - resize_width)/2
+            noise_size = (height, padding)
+            if channels > 1:
+                noise_size += (channels,)
+            noise = np.random.randint(0, 255, noise_size).astype('uint8')
+            image = np.concatenate((noise, image, noise), axis=1)
+
     if flip and random.randint(0, 1) == 0:
         return image[:, :, ::-1]
     else:
         return image
 
-def resize_image(image_path):
-    output_side_length = 256
-    img = cv2.imread(image_path)
-    height, width, depth = img.shape
-    new_height = output_side_length
-    new_width = output_side_length
-    if height > width:
-        new_height = output_side_length * height / width
-    else:
-        new_width = output_side_length * width / height
-    resized_img = cv2.resize(img, (new_width, new_height))
-    height_offset = (new_height - output_side_length) / 2
-    width_offset = (new_width - output_side_length) / 2
-    cropped_img = resized_img[height_offset:height_offset + output_side_length, width_offset:width_offset + output_side_length]
-    return cropped_img
 
-def inspect(image_path, mean, model_path, label, network, gpu=-1):
+def inspect(image_path, mean, model_path, label, network, resize_mode, channels, gpu=-1):
     network = network.split(os.sep)[-1]
     model_name = re.sub(r"\.py$", "", network)
     model_module = load_module(os.path.dirname(model_path), model_name)
@@ -64,9 +140,16 @@ def inspect(image_path, mean, model_path, label, network, gpu=-1):
         cuda.check_cuda_available()
         cuda.get_device(gpu).use()
         model.to_gpu()
-    cropwidth = 256 - model.insize
-    img = read_image(image_path, model, mean_image, cropwidth)
-    x = np.ndarray((1, 3, model.insize, model.insize), dtype=np.float32)
+        
+    output_side_length = model.insize
+        
+    img = read_image(image_path, output_side_length, output_side_length, resize_mode,channels)
+    img = img.transpose(2, 0, 1)
+    img = img.astype(np.float32)
+    img -= mean_image
+
+    x = np.ndarray((1, channels,  output_side_length, output_side_length), dtype=np.float32)
+
     x[0] = img
     if gpu >= 0:
         x = cuda.to_gpu(x)
@@ -92,8 +175,12 @@ if __name__ == '__main__':
                          help='Path to the mean file (downloaded from DEEPstation)')
     parser.add_argument('--gpu', '-g', default=-1, type=int,
                         help='GPU ID (negative value indicates CPU)')
+    parser.add_argument('--resize_mode','-r', default='squash',
+                        help='can be crop, squash, fill or half_crop')
+    parser.add_argument('--channels','-c', default='3',
+                        help='3 for RGB or 1 for grayscale')
     args = parser.parse_args()
-    results = inspect(args.image_to_inspect, args.mean, args.model, args.label, args.network, args.gpu)
+    results = inspect(args.image_to_inspect, args.mean, args.model, args.label, args.network, args.resize_mode, int(args.channels), args.gpu)
     print "{rank:<5}:{name:<40} {score}".format(rank='Rank', name='Name', score='Score')
     print "----------------------------------------------------"
     for result in results:
