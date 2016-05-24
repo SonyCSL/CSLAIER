81ff7187dc6032d370a9e245ef7b5760684aa165
Yusuke HIDESHIMA <yusuke.hideshima@uei.co.jp>
2016-05-24 17:47:36 +0900

    improve run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-export DEEPSTATION_CONFIG=$(pwd)/deepstation.cfg
+if [ -z "${DEEPSTATION_CONFIG+x}" ] ; then
+    export DEEPSTATION_CONFIG=$(pwd)/deepstation.cfg
+fi
 python $(pwd)/src/main.py
